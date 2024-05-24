ec67d94d0664ca7a41afdb910d909ab5090d4ffe
NovikovRN <nightprofunkap@gmail.com>
2024-05-24 13:14:23 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   hipsi.py

--- hipsi.py
+++ hipsi.py
@@ -11,5 +11,5 @@ if table:
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x, y=y, name='Испытания', mode='markers', marker=dict(color='red', size=10)))
     fig.add_trace(go.Scatter(x=np.linspace(min(x), max(x), 50), y=polynome(np.linspace(min(x), max(x), 50)), name='Полином', mode='lines', marker=dict(color='blue')))
-    fig.update_layout(title='', xaxis_title='ϕ', yaxis_title='ψs', showlegend=True, width=720, height=500, legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99), hovermode="x unified", hoverlabel=dict(bgcolor="white", font_color="black"), title_x=0.5, title_xanchor="center", title_yanchor="top", title_y=0.99, xaxis=dict(showgrid=True, gridwidth=.5, dtick=0.0125), yaxis=dict(showgrid=True, gridwidth=.5, dtick=0.1))
+    fig.update_layout(title='', xaxis_title='ϕ', yaxis_title='ψs', showlegend=True, width=710, height=500, legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99), hovermode="x unified", hoverlabel=dict(bgcolor="white", font_color="black"), title_x=0.5, title_xanchor="center", title_yanchor="top", title_y=0.99, xaxis=dict(showgrid=True, gridwidth=.5, dtick=0.0125), yaxis=dict(showgrid=True, gridwidth=.5, dtick=0.1))
     st.plotly_chart(fig)
