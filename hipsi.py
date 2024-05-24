6e8f631b8994ba50829e7e5eee7d19deec8aaba3
NovikovRN <nightprofunkap@gmail.com>
2024-05-24 11:30:44 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   hipsi.py

--- hipsi.py
+++ hipsi.py
@@ -1,26 +1,17 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
+import streamlit as st, pandas as pd, numpy as np, plotly.graph_objects as go
 st.set_page_config(layout="wide")
 table = st.file_uploader("Таблицу с фи и пси сюда")
 if table:
     df = pd.read_excel(table)
-    x = df["Fi"]
-    y = df["Psis"]
-
+    x, y = df["Fi"], df["Psis"]
     polynome_power = st.number_input("Степень полинома", value=4, step=1, min_value=1)
-
     polynome = np.poly1d(np.polyfit(x, y, polynome_power))
-
     coefs = list(polynome.coefficients)
-
     poly_generate = " + ".join([f"A_{'{'+ str(i) +'}'} * \phi^{'{'+str(polynome_power - i)+'}'}" for i in range(polynome_power+1)])
     st.subheader(f"Полином:")
     st.latex("\psi_{s} = " + poly_generate)
     st.write(f"Коэффициенты:")
-    tab_co = [[f'A{i}', coefs[i]] for i in range(polynome_power+1)]
-    st.dataframe(pd.DataFrame(tab_co, columns=['коэффициент', 'значение']), hide_index=True)
+    st.dataframe(pd.DataFrame([[f'A{i}', coefs[i]] for i in range(polynome_power+1)], columns=['коэффициент', 'значение']), hide_index=True)
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x, y=y, name='Испытания', mode='markers', marker=dict(color='red', size=10)))
     fig.add_trace(go.Scatter(x=x, y=polynome(x), name='Полином', mode='lines', marker=dict(color='blue')))
